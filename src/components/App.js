e8c34fe4e4d523f5945de9ab17fa44e41c8d8125
Jiho Bok <bok.jiho@gmail.com>
2022-07-27 16:32:46 +0900

    [CHORE] styled-components 설치 및 Reset CSS 적용

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Router from "./Router";
 import { authService } from "fbase";
+import styled from "styled-components";
 
 function App() {
   const [init, setInit] = useState(false);
@@ -19,14 +20,85 @@ function App() {
     });
   }, []);
   return (
-    <>
+    <GlobalStyle>
       {init ? (
         <Router isLoggedIn={isLoggedIn} userInfo={userInfo} />
       ) : (
         "Initializing..."
       )}
-    </>
+    </GlobalStyle>
   );
 }
+
+const GlobalStyle = styled.div`
+  /*! minireset.css v0.0.6 | MIT License | github.com/jgthms/minireset.css */
+  blockquote,
+  body,
+  dd,
+  dl,
+  dt,
+  fieldset,
+  figure,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  hr,
+  html,
+  iframe,
+  legend,
+  li,
+  ol,
+  p,
+  pre,
+  textarea,
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-size: 100%;
+    font-weight: 400;
+  }
+  ul {
+    list-style: none;
+  }
+  button,
+  input,
+  select {
+    margin: 0;
+  }
+  html {
+    box-sizing: border-box;
+  }
+  *,
+  :after,
+  :before {
+    box-sizing: inherit;
+  }
+  img,
+  video {
+    height: auto;
+    max-width: 100%;
+  }
+  iframe {
+    border: 0;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  td,
+  th {
+    padding: 0;
+  }
+`;
 
 export default App;

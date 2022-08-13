917f31f386b8beb247404f5f2a3c14c5616e35c7
Jiho Bok <bok.jiho@gmail.com>
2022-08-13 22:44:01 +0900

    [FEAT] 페이지 이동시 네비게이션 바에서 현재 탭 표시

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,32 @@
 import { authService } from "fbase";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Icon } from "@iconify/react";
 
 function Navigation() {
+  const [activeTab, setActiveTab] = useState(1);
   const navigate = useNavigate();
   const onClickHandler = () => {
     authService.signOut();
     navigate("/");
   };
+
+  useEffect(() => {
+    switch (window.location.pathname) {
+      case "/":
+        setActiveTab(1);
+        break;
+      case "/library":
+        setActiveTab(2);
+        break;
+      case "/notes":
+        setActiveTab(3);
+        break;
+      default:
+        setActiveTab(1);
+    }
+  });
 
   return (
     <StyledNav>
@@ -19,19 +36,19 @@ function Navigation() {
             <img src={process.env.PUBLIC_URL + "logo.png"} alt="logo icon" />
           </span>
         </li>
-        <li>
+        <li className={activeTab === 1 ? "active" : ""}>
           <Link to="/">
             <Icon icon="bx:search-alt-2" />
             Search
           </Link>
         </li>
-        <li>
+        <li className={activeTab === 2 ? "active" : ""}>
           <Link to="/library">
             <Icon icon="mdi:bookshelf" />
             Library
           </Link>
         </li>
-        <li>
+        <li className={activeTab === 3 ? "active" : ""}>
           <Link to="/notes">
             <Icon icon="fa-solid:pen" />
             Notes
@@ -53,22 +70,24 @@ const StyledNav = styled.nav`
     padding: 15px 0;
   }
   li {
-    height: 28px;
-    font-size: 22px;
-    font-weight: 500;
+    height: 22px;
+    font-size: 20px;
   }
   li:first-child {
     height: auto;
+  }
+  .active a {
+    color: #233142;
+    font-weight: 500;
   }
   img {
     width: 130px;
     height: auto;
   }
   a {
-    line-height: 22px;
+    line-height: 20px;
     color: #6d8fad;
     display: inline-flex;
-    /* align-items: center; */
 
     svg {
       margin-right: 8px;
@@ -76,4 +95,4 @@ const StyledNav = styled.nav`
   }
 `;
 
-export default Navigation;
+export default React.memo(Navigation);

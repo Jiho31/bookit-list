2489639ed8b152fce74740a01f2e3d67cb509ad9
Jiho Bok <bok.jiho@gmail.com>
2022-08-24 11:22:45 +0900

    [CHORE] 각 페이지에 사용자 로그인 정보 전달하도록 수정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Library from "routes/Library";
-import Notes from "routes/Notes";
+import Memos from "routes/Memos";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 import Navigation from "./Navigation";
@@ -16,8 +16,11 @@ function Router({ isLoggedIn, userInfo }) {
         ) : (
           <Route path="/" element={<Auth />}></Route>
         )}
-        <Route path="/library" element={<Library />}></Route>
-        <Route path="/notes" element={<Notes />} userInfo={userInfo}></Route>
+        <Route
+          path="/library"
+          element={<Library userInfo={userInfo} />}
+        ></Route>
+        <Route path="/memos" element={<Memos userInfo={userInfo} />}></Route>
       </Routes>
     </BrowserRouter>
   );
